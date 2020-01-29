aba9b879af4dd48178d3df9a6a66baba7534cf83
berkcoven <59045596+berkcoven@users.noreply.github.com>
2020-01-29 10:33:04 +0300

    Most Watched

--- MovieRent.BLL/Repositories/MoviesRepository.cs
+++ MovieRent.BLL/Repositories/MoviesRepository.cs
@@ -1,4 +1,6 @@
-﻿using MovieRent.Entities;
+﻿using MovieRent.BLL.Singleton;
+using MovieRent.DAL;
+using MovieRent.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,23 @@ namespace MovieRent.BLL.Repositories
 {
     public class MoviesRepository:BaseRepository<Movies>
     {
-        
+        private ProjectContext db;
+        public MoviesRepository()
+        {
+            db = DbTool.DbInstance;
+        }
+        public void KiralaTarihi(List<UserAppFilmList> uap,DateTime dt)
+        {
+
+            foreach (UserAppFilmList item in uap)
+            {
+                item.KiralaTarihi = dt;
+                db.SaveChanges();
+            }
+           
+        }
+
+
+
     }
 }
